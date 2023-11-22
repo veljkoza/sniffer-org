a461905da43769aa17dc2de52967fc7c78e42d29
veljkoza <veljko.sekulic1998@gmail.com>
2023-11-22 20:43:09 +0100

    revrite

--- apps/extension/services/frontend-services.ts
+++ apps/extension/services/frontend-services.ts
@@ -2,7 +2,8 @@ import { frontendServicesFactory } from '@sniffer/frontend-services';
 const test = import.meta as any;
 const env = test.env;
 console.log({ test });
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://sniffer-api.onrender.com';
+// const API_URL = 'http://localhost:5000';
 const frontendServices = frontendServicesFactory(API_URL);
 
 export { frontendServices };
